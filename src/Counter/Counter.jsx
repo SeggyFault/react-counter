308a725472a4db3e919dbb464764cb1f84d358af
sggyflt <sgflt88@gmail.com>
2024-09-01 16:16:25 +0200

    feat: add Counter component functionality

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,9 +1,31 @@
 import styles from "./Counter.module.css";
+import {useState} from "react";
 
 function Counter() {
+
+  const [counter, setCounter] = useState(0);
+
+  const decrementCounter = () => {
+    counter <= 0 ? setCounter(0) : setCounter(counter - 1);
+  }
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  }
+
+  const resetCounter = () => {
+    setCounter(0);
+  }
+
   return(
-    <div className={styles.counter}>
+    <div className={styles.counterBlock}>
       <h1>Counter</h1>
+      <span className={styles.counter}>{counter}</span>
+      <div className={styles.counterButtons}>
+        <button onClick={decrementCounter} className={styles.decrementButton}>-</button>
+        <button onClick={resetCounter} className={styles.resetButton}>Reset</button>
+        <button onClick={incrementCounter} className={styles.incrementButton}>+</button>
+      </div>
     </div>
   )
 }
