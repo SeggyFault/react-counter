c967d972938b79380cb200ca36592e02f8a48540
Oleksandra Masiukevych <60195328+SeggyFault@users.noreply.github.com>
2025-04-07 11:08:14 +0200

    feat: add redux for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
+import { store } from './store';
 import Counter from "./Counter/Counter.jsx";
 
 function App() {
 
   return (
     <>
+    <Provider store={store}>
       <Counter/>
+    </Provider>
     </>
   )
 }
--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,41 +1,23 @@
 import styles from "./Counter.module.css";
-import {useState, useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "../store/counterSlice";
 
 function Counter() {
+  const counter = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-  const [counter, setCounter] = useState(0);
-  const [color, setColor] = useState("#0277BD");
-
-  useEffect(() => {
-    if (counter < 0) {
-      setColor(c => c = "#BF360C");
-    } else if (counter > 0) {
-      setColor(c => c = "#2E7D32");
-    } else {
-      setColor(c => c = "#0277BD");
-    }
-  }, [counter, color]);
-
-  const decrementCounter = () => {
-    setCounter(c => c - 1);
-  }
-
-  const incrementCounter = () => {
-    setCounter(c => c + 1);
-  }
-
-  const resetCounter = () => {
-    setCounter(c => c = 0);
-  }
+  let color = "#0277BD";
+  if (counter < 0) color = "#BF360C";
+  else if (counter > 0) color = "#2E7D32";
 
   return(
     <div className={styles.counterBlock}>
       <h1>Counter</h1>
-      <span className={styles.counter} style={{color: color}}>{counter}</span>
+      <span className={styles.counter} style={{color}}>{counter}</span>
       <div className={styles.counterButtons}>
-        <button onClick={decrementCounter} className={styles.decrementButton}>-</button>
-        <button onClick={resetCounter} className={styles.resetButton}>Reset</button>
-        <button onClick={incrementCounter} className={styles.incrementButton}>+</button>
+        <button onClick={() => dispatch(decrement())} className={styles.decrementButton}>-</button>
+        <button onClick={() => dispatch(reset())} className={styles.resetButton}>Reset</button>
+        <button onClick={() => dispatch(increment())} className={styles.incrementButton}>+</button>
       </div>
     </div>
   )
